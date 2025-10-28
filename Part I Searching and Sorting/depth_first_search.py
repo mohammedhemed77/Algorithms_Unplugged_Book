c9064a793efe3f1568ea9ee2d18f9df5d263bd63
hemed <mohammedhemed33@yahoo.com>
2025-10-28 15:33:42 +0300

    add ch7 Code

---
+++ Part I Searching and Sorting/depth_first_search.py
@@ -0,0 +1,43 @@
+# Source : https://www.geeksforgeeks.org/dsa/depth-first-search-or-dfs-for-a-graph/
+
+def dfsRec(adj, visited, s, res):
+    visited[s] = True
+    res.append(s)
+
+    # Recursively visit all adjacent vertices 
+    # that are not visited yet
+    for i in adj[s]:
+        if not visited[i]:
+            dfsRec(adj, visited, i, res)
+
+
+def dfs(adj):
+    visited = [False] * len(adj)
+    res = []
+    dfsRec(adj, visited, 0, res)
+    return res
+
+def addEdge(adj, u, v):
+    adj[u].append(v)
+    adj[v].append(u)
+    
+    
+if __name__ == "__main__":
+    V = 5
+    adj = []
+    
+    # creating adjacency list
+    for i in range(V):
+        adj.append([])
+        
+    addEdge(adj, 1, 2)
+    addEdge(adj, 1, 0)
+    addEdge(adj, 2, 0)
+    addEdge(adj, 2, 3)
+    addEdge(adj, 2, 4)
+
+    # Perform DFS starting from vertex 0
+    res = dfs(adj)
+
+    for node in res:
+        print(node, end=" ")
